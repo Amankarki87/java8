e9d5f1b41b8fe13d6316ecae3e9720f1e21df49a
Amankarki87 <amankarki87@gmail.com>
2024-04-23 21:45:01 +0545

    Add consumer function

--- lambda/consumer/ConsumerTest.java
+++ lambda/consumer/ConsumerTest.java
@@ -1,5 +1,6 @@
 package lambda.consumer;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 // Consumer are functional interface that takes in a parameter and do not produce anything.
@@ -9,5 +10,8 @@ public class ConsumerTest {
         Consumer<String> stringConsumer = s -> System.out.println(s);
         stringConsumer.accept("Hello World.");
         System.out.println(stringConsumer);
+
+        BiConsumer<String,String> greet = (s1,s2) -> System.out.println(s1 + s2);
+        greet.accept("Hey","World");
     }
 }
